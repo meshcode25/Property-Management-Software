b9d5d3f49a699e0a318df9016e658c10d6e54e8a
Yegon Kiprotich <meshcode75@gmail.com>
2022-08-23 19:10:03 +0300

    About to finish Access and refresh tokens

--- pmsclient/src/components/VerifyComponent.js
+++ pmsclient/src/components/VerifyComponent.js
@@ -3,29 +3,18 @@ import { useParams } from "react-router-dom"
 //  import styled from "styled-components"
   import {verify} from "./Auth.services"
 
-
-
-
-
 const VerifyEmail=()=>{
     var params = useParams()
 
     verify(params.verificationcode).then((response)=>{
         if(response.status===201){
             console.log(response)
-            console.log("I can do the verification")
-            // return console.log("if this works, I think we are don here")
              return window.location.href=(`/verifysuccess`)
         }
-
-
         else{
-
             return window.location.href="/verifyfail"
 
         }
-
-
     }).catch(err=>console.error(err))
     
    
