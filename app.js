b65c823c37b4fb3f710e83fab20b2e594d7581f6
Yegon Kiprotich <meshcode75@gmail.com>
2023-04-08 23:39:09 +0300

    hope both react and node will work this time round fingers crossed again and again and again and again and again and again and again amd again AND AGAIN and again and again and again and again and again and again and again

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ app.use(express.static(clientpath));
 */
 
 
-const publicPath = "./pmsclient/public"
+const publicPath = path.join("PMS","./pmsclient/public")
 
 app.use(express.static(path.join(publicPath, "index.html")))
 
