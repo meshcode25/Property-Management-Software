d37d12ccb93795491b798cd1301869bc55be9154
Yegon Kiprotich <meshcode75@gmail.com>
2023-04-09 09:35:37 +0300

    hope both react and node will work this time round fingers crossed again and again and again and again and again and again and again amd again AND AGAIN and again and again and again and again and again and again and again

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ app.use(express.static(clientpath));
 */
 
 
-const publicPath = path.join(__dirname,"public")
+const publicPath = path.join(__dirname,"public/build")
 
 app.use(express.static(path.join(publicPath, "index.html")))
 
