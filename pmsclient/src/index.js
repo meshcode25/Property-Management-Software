a338698cb1804b4cf0c93f2fbc6f310a38dc46e8
Yegon Kiprotich <meshcode75@gmail.com>
2023-04-11 12:36:24 +0300

    now want to just render react app

--- pmsclient/src/index.js
+++ pmsclient/src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App.js';
 import './index.css';
 
+import { hydrateRoot } from 'react-dom/client';
+
+const domNode = document.getElementById('root');
+const root = hydrateRoot(domNode, reactNode);
+
+hydrateRoot(
+  document.getElementById('root'),
+  <App />
+);
 
 
+/*
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+*/
